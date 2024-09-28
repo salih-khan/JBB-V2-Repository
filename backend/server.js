7c48ec1de2152bcf1a17c526545c5ca8de8e5960
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2024-09-28 20:52:49 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -85,6 +85,11 @@ const startServer = async () => {
     //   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     // });
 
+
+    app.get('/*', (req, res) => {
+      res.redirect(`https://jbb-frontend.onrender.com${req.originalUrl}`);
+    });
+    
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
