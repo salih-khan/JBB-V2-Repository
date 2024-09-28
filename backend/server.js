7e68d8afc16f2b1d4985769efd96331b04890ed0
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2024-09-28 14:31:44 +0100

    port changed
    Port changed to work with render

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ const startServer = async () => {
       res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
 
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Server is running on port ${PORT}`);
     });
 
