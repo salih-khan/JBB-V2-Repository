34b7399ab7f8bc6859ada7a59c8bafce0b1a25f0
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2024-09-29 01:57:55 +0100

    Update passport.config.js

--- backend/config/passport.config.js
+++ backend/config/passport.config.js
@@ -1,24 +1,22 @@
 const passport = require('passport');
-const { Strategy } = require('passport-google-oauth20');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
 const User = require('../models/user.models');
 const { verifyCallbackFunction } = require('../controllers/auth.controllers');
 
 const AUTH_OPTIONS = {
     callbackURL: 'https://jbb-fullstack.onrender.com/auth/google/callback',
     clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-}; 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallbackFunction));
+    clientSecret: process.env.CLIENT_SECRET,
+};
+
+passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallbackFunction));
 
 passport.serializeUser((user, done) => {
-    done(null, user.id); // Store user ID in the session
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
-        done(null, user);
-    }).catch(err => {
-        console.error('Error deserializing user', err);
-        done(err, null);
-    });
+    User.findById(id)
+        .then((user) => done(null, user))
+        .catch((err) => done(err, null));
 });
