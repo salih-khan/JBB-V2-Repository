08fadc96bb9e9efb21a2ce84c3f4f486e14ba2c6
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2024-10-04 23:19:58 +0100

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -15,6 +15,8 @@ router.get('/auth/google/callback', passport.authenticate('google', {
     console.log("Google called us back");
     console.log('Google OAuth success, user:', req.user);  // Check if user is authenticated
     console.log('Session:', req.session);  // Check if session is being created
+    res.redirect('/');  // Redirect to your desired route after successful login
+
 });
 
 router.get('/auth/logout', (req, res) => {
