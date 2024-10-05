5c833d76769994a61be11e5100a458c6f2fb80e3
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2024-10-05 23:10:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,14 +10,9 @@ self.addEventListener('activate', (event) => {
 
 // Fetch event listener
 self.addEventListener('fetch', (event) => {
-    // Only respond to requests for your domain
-    if (event.request.url.includes('jbb.foundation')) {
-        event.respondWith(
-            fetch(event.request).catch((error) => {
-                console.error('Fetching failed:', error);
-                // Optionally return a fallback response or cache hit
-            })
-        );
-    }
-    // For other requests (like Google Ads), fall back to the network
-});
+    event.respondWith(
+        fetch(event.request).catch((error) => {
+            console.error('Fetching failed:', error);
+            throw error; // Let the browser handle the error
+        })
+    );
